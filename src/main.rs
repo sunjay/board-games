18da7d180d3990be9a38116eb0f116f77300d0db
Sunjay Varma <varma.sunjay@gmail.com>
2020-02-23 15:09:51 -0500

    Starting from scratch

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,2 @@
-mod token;
-mod expr;
-mod parser;
-
-use std::error::Error;
-
-use token::{Token, TokenStream};
-use expr::Expr;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    use Token::*;
-    let tokens = vec![Int(32), Plus, LeftParen, Int(-24), Minus, Int(10), RightParen];
-    let mut token_stream = TokenStream::new(tokens);
-
-    let expr = Expr::parse(&mut token_stream)?;
-    println!("{:#?}", expr);
-
-    Ok(())
+fn main() {
 }
